f7f013c3953dd396dfe5c65c38a5ae633f590835
Samuel Gasc <contact@samuelgasc.fr>
2020-02-05 13:50:45 +0100

    Update captchaVerification.php

--- test/captchaVerification.php
+++ test/captchaVerification.php
@@ -3,17 +3,17 @@
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-	if (isset($_POST['myCaptchaFieldForExample']) && $_POST['myCaptchaFieldForExample'] === $_SESSION['captcha'])
-	{
-		echo '<h3>Congratulations, this is the right captcha!</h3><a href="index.php">Try again</a>';
-	}
-	else
-	{
-		echo '<h3>Wrong captcha.</h3><a href="index.php">Try again</a>';
-	}
+    if (isset($_POST['myCaptchaFieldForExample']) && $_POST['myCaptchaFieldForExample'] === $_SESSION['captcha'])
+    {
+        echo '<h3>Congratulations, this is the right captcha!</h3><a href="index.php">Try again</a>';
+    }
+    else
+    {
+        echo '<h3>Wrong captcha.</h3><a href="index.php">Try again</a>';
+    }
 }
 else
-{	
-	header("Location: index.php");
+{   
+    header("Location: index.php");
     exit;
 }
