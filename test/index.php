3b2a84b711e561825ddd8fc96e812ed0445e639d
Samuel Gasc <contact@samuelgasc.fr>
2020-02-05 13:51:33 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -21,49 +21,49 @@ $pathToCaptcha = '../captchaFile/';
 
 try
 {
-	// Instantiates the captcha object (will do all the captcha's creation job)
-	$captchaBuilder = new Captcha($pathToCaptcha);
+    // Instantiates the captcha object (will do all the captcha's creation job)
+    $captchaBuilder = new Captcha($pathToCaptcha);
 
-	// creates the captcha
-	if (true === $captchaBuilder->getCaptcha(8))
-	{
-		$captchaFileName = $_SESSION['captchaPngFileName'];
+    // creates the captcha
+    if (true === $captchaBuilder->getCaptcha(8))
+    {
+        $captchaFileName = $_SESSION['captchaPngFileName'];
 
-		?>
+        ?>
 
-		<!-- EXAMPLE OF CAPTCHA USE -->
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Test captcha</title>
-		</head>
-		<body>
-			<?php
-				/* !!!!! It goes obvious that this is a HUGE security leak to display the following to the user  !!!!! */
-				/* !!!!!          DO NOT FORGET TO REMOVE IT FROM YOUR CODE WHEN STEPING TO PRODUCTION           !!!!! */
+        <!-- EXAMPLE OF CAPTCHA USE -->
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Test captcha</title>
+        </head>
+        <body>
+            <?php
+                /* !!!!! It goes obvious that this is a HUGE security leak to display the following to the user  !!!!! */
+                /* !!!!!          DO NOT FORGET TO REMOVE IT FROM YOUR CODE WHEN STEPING TO PRODUCTION           !!!!! */
 
-				print_r('<pre>');
-				var_dump($_SESSION['captcha']);
-				print_r('</pre>');
+                print_r('<pre>');
+                var_dump($_SESSION['captcha']);
+                print_r('</pre>');
 
-				/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
-			?>
-			<img src="<?= $pathToCaptcha.$captchaFileName; ?>" width="250" height="auto" />
-			<hr />
-			<form method="POST" action="captchaVerification.php">
-				<p>
-					<input type="text" name="myCaptchaFieldForExample" placeholder="Write the captcha's value here." /><br/>
-					<input type="submit">
-				</p>
-			</form>
-		</body>
-		</html>
+                /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+            ?>
+            <img src="<?= $pathToCaptcha.$captchaFileName; ?>" width="250" height="auto" />
+            <hr />
+            <form method="POST" action="captchaVerification.php">
+                <p>
+                    <input type="text" name="myCaptchaFieldForExample" placeholder="Write the captcha's value here." /><br/>
+                    <input type="submit">
+                </p>
+            </form>
+        </body>
+        </html>
 
-	<?php
-	}
+    <?php
+    }
 
 }
 catch (Exception $e)
 {
-	echo "<h3>" . $e->getMessage() . "</h3>";
+    echo "<h3>" . $e->getMessage() . "</h3>";
 }
