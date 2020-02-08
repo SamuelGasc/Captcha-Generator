2c7675eaba700dcaebab47b9971abb0728a3d203
Samuel Gasc <contact@samuelgasc.fr>
2020-02-08 18:02:23 +0100

    Update captchaVerification.php

--- test/captchaVerification.php
+++ test/captchaVerification.php
@@ -1,19 +1,18 @@
 <?php
-// Basic server side verification (if the user writes the good captcha, then the form is validated)
 session_start();
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST')
 {
-    if (isset($_POST['myCaptchaFieldForExample']) && $_POST['myCaptchaFieldForExample'] === $_SESSION['captcha'])
-    {
-        echo '<h3>Congratulations, this is the right captcha!</h3><a href="index.php">Try again</a>';
-    }
-    else
-    {
-        echo '<h3>Wrong captcha.</h3><a href="index.php">Try again</a>';
-    }
-}
-else
-{   
     header("Location: index.php");
     exit;
+}
+
+// Basic server side verification (if the user writes the good captcha, then the form is validated)
+if (isset($_POST['myCaptchaFieldForExample']) && $_POST['myCaptchaFieldForExample'] === $_SESSION['captcha'])
+{
+    echo '<h3>Congratulations, this is the right captcha!</h3><a href="index.php">Try again</a>';
+}
+else
+{
+    echo '<h3>Wrong captcha.</h3><a href="index.php">Try again</a>';
 }
